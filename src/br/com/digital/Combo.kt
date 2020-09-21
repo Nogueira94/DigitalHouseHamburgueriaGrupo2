5be33056399119abead558fc9918df9d8adeaad7
Peterson <pepe419@gmail.com>
2020-09-21 20:47:24 -0300

    Peterson Classe combo

--- src/br/com/digital/Combo.kt
+++ src/br/com/digital/Combo.kt
@@ -1,4 +1,13 @@
 package br.com.digital
 
-class Combo {
+class Combo(val listaProdutos: MutableList<Produto>, val desconto: Float, val nome: String) {
+    var preco: Float = 0.0f
+
+    init {
+        listaProdutos.forEach {
+            preco += it.preco * (1 - desconto)
+        }
+
+        println("O valor total do combo é R$: $preco")
+    }
 }
